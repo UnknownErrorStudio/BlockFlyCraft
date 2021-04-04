994ea9b7d482fd26a21921ed0cd66a81bdc49322
286799714 <41233300+286799714@users.noreply.github.com>
2021-04-04 11:49:29 +0800

    项目初始化

---
+++ src/main/java/cn/unknownerror/blockflycraft/BlockFlyCraft.kt
@@ -0,0 +1,20 @@
+package cn.unknownerror.blockflycraft
+
+import io.izzel.taboolib.TabooLib
+import io.izzel.taboolib.loader.Plugin
+
+object BlockFlyCraft : Plugin() {
+
+
+    override fun onLoad() {
+
+    }
+
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
